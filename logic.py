32d4bcf2b300e636b63b553295f261349cf7fbcb
Mikalai.Melchanka <mikalai.melchanka@unilever.com>
2024-12-10 12:22:42 +0100

    Assertion functions

--- logic.py
+++ logic.py
@@ -1,13 +1,25 @@
 import clips
 
-environment = clips.Environment()
+
+def assert_template(env, tmpl_name, fields: dict):
+    tmpl = env.find_template(tmpl_name)
+    tmpl.assert_fact(**fields)
+
+
+def assert_question(env, question_text, answers):
+    assert_template(env, 'question', {'text': question_text, 'valid-answers': answers})
+
+
+def assert_answer(env, answer, question_text):
+    assert_template(env, 'answer', {'answer-given': answer, 'to-question': question_text})
+
 
 if __name__ == '__main__':
+    environment = clips.Environment()
     environment.load(path="baza.clp")
-
-    # retrieve the fact template
     environment.reset()
 
+    assert_template(environment, 'person', {'name': 'Test', 'surname': '22'})
     # execute the activations in the agenda
     environment.run()
 
